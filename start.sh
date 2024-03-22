6ebb987812e54c435ceb926fc76e7599ff14eea9
Sven <scysys@outlook.com>
2024-03-22 17:54:05 +0100

    0.9.8

--- start.sh
+++ start.sh
@@ -9,5 +9,9 @@ rm -f /home/spcast/SPCast/news/logs/*.log*
 # Set permissions
 chmod +x spcast_news.liq
 
+# Replace the value in news_credentials_alternate.liq
+source_password=$(cat /usr/share/zabbix/sp/secure/autodj_source_password.txt.php)
+sed -i "s/spChangeToAutoDJSourcePassword/$source_password/" /home/spcast/SPCast/news/news_credentials_alternate.liq
+
 # Execute the spcast_news.liq with the name sitmjlxvvpfimx
 bash -c './spcast_news.liq --name "sitmjlxvvpfimx"' > /dev/null 2>&1
